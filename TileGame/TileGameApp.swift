103917cd66bbb03bae7a6b5626745392d3467ca6
Danilo Miranda Gusicuma <danilo.m.gusicuma@gmail.com>
2022-01-21 11:33:19 -0300

    initial commit

---
+++ TileGame/TileGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TileGameApp.swift
+//  TileGame
+//
+//  Created by Danilo Miranda Gusicuma on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TileGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SpriteKitContainer()
+        }
+    }
+}
