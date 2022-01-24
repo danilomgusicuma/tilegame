cb1dd0a45f253949ab3ee5b89ed62532d204187e
Cássio Dezotti <cassiodezotti@gmail.com>
2022-01-24 12:08:05 -0300

    Primeiro teste drag and drop, sem lugar fixo

--- TileGame/ContentView.swift
+++ TileGame/ContentView.swift
@@ -9,62 +9,67 @@ import SwiftUI
 import SpriteKit
 
 class GameScene: SKScene {
+    var pieces = [SKSpriteNode]()
+    var currentNode: SKNode?
+    
+    override func sceneDidLoad() {
+        for _ in 0...5 {
+            let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+            pieces.append(box)
+        }
+        for i in 1...pieces.count - 1 {
+            
+            pieces[i].position = CGPoint(x: CGFloat( i * 70), y: CGFloat(60))
+            addChild(pieces[i])
+        }
+        
+        
+    }
     override func didMove(to view: SKView) {
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first, let node = self.currentNode {
+            let touchLocation = touch.location(in: self)
+            node.position = touchLocation
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        let location = touch.location(in: self)
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
-        box.position = location
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        addChild(box)
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            
+            let touchedNodes = self.nodes(at: location)
+            for node in touchedNodes.reversed() {
+                self.currentNode = node
+                print(self.currentNode?.position)
+            }
+        }
+        
     }
 }
-
-struct SpriteKitContainer: View {
     
-    var scene: SKScene {
-        let scene = GameScene()
-        scene.size = CGSize(width: 300, height: 400)
-        scene.scaleMode = .fill
-        return scene
-    }
-    
-    var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Spacer()
-                SpriteView(scene: scene)
-                    .frame(width: geometry.size.width, height: (4/3) * geometry.size.width)
-                Spacer()
+    struct SpriteKitContainer: View {
+        
+        var scene: SKScene {
+            let scene = GameScene()
+            scene.size = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+            scene.scaleMode = .fill
+            return scene
+        }
+        
+        var body: some View {
+            GeometryReader { geometry in
+                VStack {
+                    Spacer()
+                    SpriteView(scene: scene)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .clipped()
+                    Spacer()
+                }
             }
         }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -75,3 +80,5 @@ struct ContentView_Previews: PreviewProvider {
         SpriteKitContainer()
     }
 }
+
+
