d77f1b97268d8b6394c7319851b80ff3412b5096
Cássio Dezotti <cassiodezotti@gmail.com>
2022-01-25 14:49:22 -0300

    Continuando testes para drag and drop em local especifico

--- TileGame/ContentView.swift
+++ TileGame/ContentView.swift
@@ -9,18 +9,54 @@ import SwiftUI
 import SpriteKit
 
 class GameScene: SKScene {
+    var formBins = [SKSpriteNode]()
+    var piecesBins = [SKSpriteNode]()
     var pieces = [SKSpriteNode]()
     var currentNode: SKNode?
     
     override func sceneDidLoad() {
-        for _ in 0...5 {
-            let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
-            pieces.append(box)
-        }
-        for i in 1...pieces.count - 1 {
+        var slot = SKSpriteNode(color: .gray, size: CGSize(width: 100, height: 100))
+        
+        formBins.append(slot)
+        formBins[0].position = CGPoint(x: CGFloat( UIScreen.main.bounds.size.width/2 - 55), y: CGFloat(UIScreen.main.bounds.size.height/2 + 120))
+        formBins[0].name = "formSlot"
+        addChild(formBins[0])
+        
+        
+        slot = SKSpriteNode(color: .gray, size: CGSize(width: 100, height: 100))
+        formBins.append(slot)
+        formBins[1].position = CGPoint(x: CGFloat( UIScreen.main.bounds.size.width/2 + 55), y: CGFloat(UIScreen.main.bounds.size.height/2 + 120))
+        formBins[1].name = "formSlot"
+        addChild(formBins[1])
+        
+        slot = SKSpriteNode(color: .gray, size: CGSize(width: 100, height: 100))
+        formBins.append(slot)
+        formBins[2].position = CGPoint(x: CGFloat( UIScreen.main.bounds.size.width/2 - 55), y: CGFloat(UIScreen.main.bounds.size.height/2 + 10))
+        formBins[2].name = "formSlot"
+        addChild(formBins[2])
+        
+        slot = SKSpriteNode(color: .gray, size: CGSize(width: 100, height: 100))
+        formBins.append(slot)
+        formBins[3].position = CGPoint(x: CGFloat( UIScreen.main.bounds.size.width/2 + 55), y: CGFloat(UIScreen.main.bounds.size.height/2 + 10))
+        formBins[3].name = "formSlot"
+        addChild(formBins[3])
+
+        
+        for i in 0...5 {
+            var box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
             
-            pieces[i].position = CGPoint(x: CGFloat( i * 70), y: CGFloat(60))
+            
+            piecesBins.append(box)
+            piecesBins[i].position = CGPoint(x: CGFloat( i * 55 + 55), y: CGFloat(60))
+            piecesBins[i].name = "initialSlot" + String(i)
+            
+            box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+            pieces.append(box)
+            pieces[i].position = CGPoint(x: CGFloat( i * 55 + 55), y: CGFloat(60))
+            pieces[i].name = "piece" + String(i)
             addChild(pieces[i])
+            
+            
         }
         
         
@@ -42,34 +78,53 @@ class GameScene: SKScene {
             let touchedNodes = self.nodes(at: location)
             for node in touchedNodes.reversed() {
                 self.currentNode = node
-                print(self.currentNode?.position)
             }
         }
         
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.currentNode?.name?.contains("piece") != nil{
+            if formBins[0].frame.contains(self.currentNode?.position ?? CGPoint(x: 0, y: 0)){
+                print("alou1")
+            }else if formBins[1].frame.contains(self.currentNode?.position ?? CGPoint(x: 0, y: 0)){
+                print("alou2")
+            }else if formBins[2].frame.contains(self.currentNode?.position ?? CGPoint(x: 0, y: 0)){
+                print("alou3")
+            }else if formBins[3].frame.contains(self.currentNode?.position ?? CGPoint(x: 0, y: 0)){
+                print("alou4")
+            }
+        }
+    }
+    
+    func returnPieces(){
+        for piece in pieces {
+            
+        }
     }
 }
+
+struct SpriteKitContainer: View {
     
-    struct SpriteKitContainer: View {
-        
-        var scene: SKScene {
-            let scene = GameScene()
-            scene.size = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
-            scene.scaleMode = .fill
-            return scene
-        }
-        
-        var body: some View {
-            GeometryReader { geometry in
-                VStack {
-                    Spacer()
-                    SpriteView(scene: scene)
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .clipped()
-                    Spacer()
-                }
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        scene.scaleMode = .fill
+        return scene
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                SpriteView(scene: scene)
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .clipped()
+                Spacer()
             }
         }
     }
+}
 
 
 
